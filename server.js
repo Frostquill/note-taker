eca4bc3c84c79bbed6afd359c28a44ba6eb7d471
Frostquill <ruiz82494@gmail.com>
2021-06-27 18:42:36 -0700

    start

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const fs = require('fs');
+const PORT = 3001;
+const app = express();
 
+app.get('/api/notes', function (req, res) {
+    res.sendFile(__dirname, './db/db.json');
+})
+
+
+
+
+app.listen(PORT, () => {
+    console.log(app);
+    console.log(`API server now on ${PORT}!`);
+});
